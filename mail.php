230a587f407ac472449624cf83d872a9837c3cbb
Seyedehroshamm <32647414+seyedehroshamm@users.noreply.github.com>
2017-12-14 00:38:32 +0330

    Delete mail.php

--- mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "seyedehroshamm619@gmail.com.com";
-$post = "My subject";
-$message = "Hello world!";
-$email = "From: hadeh0011@gmail.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-mail($to,$post,$message,$email);
-?> 
