a0a0c03de7e1b81d8b68a02b9a05b962a774590a
Seyedehroshamm <32647414+seyedehroshamm@users.noreply.github.com>
2017-12-14 02:24:55 +0330

    Create mail.php

---
+++ php/mail.php
@@ -0,0 +1,58 @@
+<?php
+$myemail = "seyedehroshamm619@gmail.com";
+
+/* Check all form inputs using check_input function */
+$fullname = check_input($_POST['name'], "Enter your name");
+$email = check_input($_POST['email']);
+$message = check_input($_POST['message'], "Write your message");
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+show_error("E-mail address not valid");
+}
+/* Let's prepare the message for the e-mail */
+$message = "
+
+Name: $fullname
+email: $email
+Message:$message
+
+";
+
+/* Send the message using mail() function */
+mail($myemail, $message,$fullname);
+
+/* Redirect visitor to the thank you page */
+header('Location: thanks.html');
+exit();
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
+}
+return $data;
+}
+
+function show_error($myError)
+{
+?>
+<html>
+<body>
+
+<p>Please correct the following error:</p>
+<strong><?php echo $myError; ?></strong>
+<p>Hit the back button and try again</p>
+
+</body>
+</html>
+<?php
+exit();
+}
+?>
