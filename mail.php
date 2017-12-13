5c133996d16d9a7adc4bfc26c188b6b7f66efe25
Seyedehroshamm <32647414+seyedehroshamm@users.noreply.github.com>
2017-12-14 00:27:28 +0330

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,20 +1,9 @@
 <?php
-//if "email" variable is filled out, send email
-  if (isset($_REQUEST['email']))  {
-  
-  //Email information
-  $admin_email = "seyedehroshamm619@gmail.com";
-  $post = $_REQUEST['NAME'];
-  $email = $_REQUEST['EMAIL'];
-  $message = $_REQUEST['submit'];
-  
-  //send email
-  mail($admin_email, "$post", $message, "From:" . $email);
-  
-  //Email response
-  echo "Thank you for contacting us!";
-  }
-  
-   "email" variable is not filled out, display the form
-  else  {
-?>
+$to = "seyedehroshamm619@gmail.com.com";
+$post = "My subject";
+$message = "Hello world!";
+$email = "From: hadeh0011@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$post,$message,$email);
+?> 
