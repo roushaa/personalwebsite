be758d0e9cb7e1f090b4d5664be53313e55cfec1
Seyedehroshamm <32647414+seyedehroshamm@users.noreply.github.com>
2017-12-13 21:24:46 +0330

    Create sendemail.php

---
+++ php/sendemail.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> sendemail</title>
+</head>
+<body>
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+ 
+$to = 'seyedehroshamm619@gmail.com';
+$subject = 'the subject';
+$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
+$headers = 'From: seyedehroshamm619@gmail.com' . "\r\n";
+ 
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+mail($to, $subject, $message, $headers); 
+echo "Your email was sent!"; 
+}else{
+echo "Invalid Email, please provide an correct email.";
+}
+?>
+</body>
+</html>
